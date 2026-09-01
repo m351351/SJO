421786fd259d45a4c5883255398ba155285da798
m351351 <turtinen.merituulia@gmail.com>
2026-09-01 11:38:18 +0300

    yhden pisteen tehtävä valmis ja aloitettu 2p tehtävää

--- src/main.c
+++ src/main.c
@@ -1,6 +1,156 @@
 #include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include "button.h"
 
+// Led pin configurations
+static const struct gpio_dt_spec red = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+static const struct gpio_dt_spec green = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
+static const struct gpio_dt_spec blue = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
+static const struct gpio_dt_spec yellow = GPIO_DT_SPEC_GET(DT_ALIAS(led3), gpios);
+
+// Red led thread initialization
+#define STACKSIZE 500
+#define PRIORITY 5
+void red_led_task(void *, void *, void*);
+void green_led_task(void *, void *, void*);
+void blue_led_task(void *, void *, void*);
+void yellow_led_task(void *, void *, void*);
+
+K_THREAD_DEFINE(red_thread,STACKSIZE,red_led_task,NULL,NULL,NULL,PRIORITY,0,0);
+K_THREAD_DEFINE(green_thread,STACKSIZE,green_led_task,NULL,NULL,NULL,PRIORITY,0,0);
+//K_THREAD_DEFINE(blue_thread,STACKSIZE,blue_led_task,NULL,NULL,NULL,PRIORITY,0,0);
+K_THREAD_DEFINE(yellow_thread,STACKSIZE,yellow_led_task,NULL,NULL,NULL,PRIORITY,0,0);
+
+
+
+// Initialize leds
+int  init_led() {
+	
+	int ret;
+	// Led pin initialization
+
+//punane
+
+		ret = gpio_pin_configure_dt(&red, GPIO_OUTPUT_ACTIVE);
+		if (ret < 0) {
+			printk("Error: Led configure failed\n");		
+			return ret;
+		}
+		// set led off
+		gpio_pin_set_dt(&red,0);
+
+//vihree
+		
+		ret = gpio_pin_configure_dt(&green, GPIO_OUTPUT_ACTIVE);
+		if (ret < 0) {
+			printk("Error: Led configure failed\n");		
+			return ret;
+		}
+		// set led off
+		gpio_pin_set_dt(&green,0);
+
+//sinine
+		ret = gpio_pin_configure_dt(&blue, GPIO_OUTPUT_ACTIVE);
+		if (ret < 0) {
+			printk("Error: Led configure failed\n");		
+			return ret;
+		}
+		// set led off
+		gpio_pin_set_dt(&blue,0);
+
+//keltane
+		ret = gpio_pin_configure_dt(&yellow, GPIO_OUTPUT_ACTIVE);
+		if (ret < 0) {
+			printk("Error: Led configure failed\n");		
+			return ret;
+		}
+		// set led off
+		gpio_pin_set_dt(&yellow,0);
+
+	printk("Led initialized ok\n");
+	
+	return 0;
+}
+
+// Main program
 int main(void)
 {
-        return 0;
+	init_led(); 
+
+	return 0;
 }
+
+int tilakone = 0; // 0=pun, 1 = kel, 2 = vih, 3=sin
+int keskeytys = false;
+
+// Task to handle red led
+void red_led_task(void *, void *, void*) {
+	
+	printk("Red led thread started\n");
+	while (true) {
+		if (tilakone == 0){
+		// 1. set led on 
+		gpio_pin_set_dt(&red,1);
+		printk("Red on\n");
+		// 2. sleep for 2 seconds
+		k_sleep(K_SECONDS(1));
+		// 3. set led off
+		gpio_pin_set_dt(&red,0);
+		printk("Red off\n");
+		// 4. sleep for 2 seconds
+		k_sleep(K_SECONDS(1));
+		tilakone = 1;
+		}
+		k_sleep(K_SECONDS(1));
+	}
+}
+
+void yellow_led_task(void *, void *, void*) {
+	
+	printk("Yellow led thread started\n");
+	while (true) {
+		if (tilakone==1){
+		// 1. set led on 
+		gpio_pin_set_dt(&red,1);
+		gpio_pin_set_dt(&green,1);
+		printk("Yellow on\n");
+		// 2. sleep for 2 seconds
+		k_sleep(K_SECONDS(1));
+		// 3. set led off
+		gpio_pin_set_dt(&red,0);
+		gpio_pin_set_dt(&green,0);
+		printk("Yellow off\n");
+		// 4. sleep for 2 seconds
+		//k_sleep(K_SECONDS(1));
+		tilakone = 2;
+		}
+		k_sleep(K_SECONDS(1));
+	}
+}
+
+void green_led_task(void *, void *, void*) {
+	
+	printk("Green led thread started\n");
+	while (true) {
+		if (tilakone==2){
+		// 1. set led on 
+		gpio_pin_set_dt(&green,1);
+		printk("Green on\n");
+		// 2. sleep for 2 seconds
+		k_sleep(K_SECONDS(1));
+		// 3. set led off
+		gpio_pin_set_dt(&green,0);
+		printk("Greem off\n");
+		// 4. sleep for 2 seconds
+		k_sleep(K_SECONDS(1));
+		tilakone = 0;
+		}
+		k_sleep(K_SECONDS(1));
+	}
+}
+
+
+
+
